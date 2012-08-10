0db00495695cd6a6fb3e1c287622ee1f980b07ac
David Heinemeier Hansson <david@loudthinking.com>
2012-08-10 18:11:51 -0500

    Fix shit (not too bad for the first stab of blind coding!)

--- lib/cache_digests/template_digestor.rb
+++ lib/cache_digests/template_digestor.rb
@@ -1,3 +1,5 @@
+require 'active_support/core_ext/array/access'
+
 module CacheDigests
   class TemplateDigestor
     EXPLICIT_DEPENDENCY = /<%# Template Dependency: ([^ ]+) %>/
--- test/template_digestor_test.rb
+++ test/template_digestor_test.rb
@@ -10,24 +10,24 @@ class FixtureTemplate
 end
 
 class FixtureFinder
-  FIXTURES_DIR = "#{File.dirname(__FILE___)}/fixtures"
-  TMP_DIR      = "#{File.dirname(__FILE___)}/tmp"
+  FIXTURES_DIR = "#{File.dirname(__FILE__)}/fixtures"
+  TMP_DIR      = "#{File.dirname(__FILE__)}/tmp"
   
-  def find(logical_name, keys, partial, formats)
-    FixtureTemplate.new("#{TMP_DIR}/#{partial ? "_" : ""}#{logical_name}.#{formats.first}.erb")
+  def find(logical_name, keys, partial, options)
+    FixtureTemplate.new("#{TMP_DIR}/#{partial ? logical_name.gsub("/", "/_") : logical_name}.#{options[:formats].first}.erb")
   end
 end
 
 class TemplateDigestorTest < MiniTest::Unit::TestCase
-  setup do
+  def setup
     FileUtils.cp_r FixtureFinder::FIXTURES_DIR, FixtureFinder::TMP_DIR
   end
   
-  teardown do
+  def teardown
     FileUtils.rm_r FixtureFinder::TMP_DIR
   end
 
-  def test_digest
+  def test_top_level_change_reflected
     assert_digest_difference("messages/show") do
       change_template("messages/show")
     end
@@ -38,15 +38,15 @@ class TemplateDigestorTest < MiniTest::Unit::TestCase
     def assert_digest_difference(template_name)
       previous_digest = digest(template_name)
       yield
-      assert_not_equal previous_digest, digest(template_name)
+      assert previous_digest != digest(template_name)
     end
   
     def digest(template_name)
-      TemplateDigestor.digest(template_name, :html, FixtureFinder.new)
+      CacheDigests::TemplateDigestor.digest(template_name, :html, FixtureFinder.new)
     end
     
     def change_template(template_name)
-      File.open("#{FixtureFinder::TMP_DIR}/#{template_name}.html.erb", "rw") do |f|
+      File.open("#{FixtureFinder::TMP_DIR}/#{template_name}.html.erb", "w") do |f|
         f.write "\nTHIS WAS CHANGED!"
       end
     end
