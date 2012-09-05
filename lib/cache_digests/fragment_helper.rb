63e29254153c1cc99fead9622419e249b7532ba0
Michael Schaefermeyer <michael.schaefermeyer@zweitag.de>
2012-09-05 16:17:35 +0200

    Refactor key generation to improve readability

--- lib/cache_digests/fragment_helper.rb
+++ lib/cache_digests/fragment_helper.rb
@@ -4,7 +4,8 @@ module CacheDigests
       # Automatically include this template's digest -- and its childrens' -- in the cache key.
       def fragment_for(key, options = nil, &block)
         if !explicitly_versioned_cache_key?(key)
-          flat_key                   = ActiveSupport::Cache.expand_cache_key(key.is_a?(Hash) ? url_for(key).split("://").last : key, :views)
+          key                        = url_for(key).split("://").last if key.is_a?(Hash)
+          flat_key                   = ActiveSupport::Cache.expand_cache_key(key, :views)
           CACHED_DIGESTS[flat_key] ||= TemplateDigestor.digest(@virtual_path, formats.last.to_sym, lookup_context)
           super [*key, CACHED_DIGESTS[flat_key]], options, &block
         else
