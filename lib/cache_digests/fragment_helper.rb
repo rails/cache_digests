569de62594ffe0e99c4c794afdad4d794a461751
Ryan Garver <ragarver@gmail.com>
2012-09-27 08:31:43 -0700

    Extract fragment_name_with_digest to stay in line with Rails 4 api

--- lib/cache_digests/fragment_helper.rb
+++ lib/cache_digests/fragment_helper.rb
@@ -1,10 +1,14 @@
 module CacheDigests
   module FragmentHelper
+    def fragment_name_with_digest(name)
+      [*key, TemplateDigestor.digest(@virtual_path, formats.last.to_sym, lookup_context)]
+    end
+
     private
       # Automatically include this template's digest -- and its childrens' -- in the cache key.
       def fragment_for(key, options = nil, &block)
         if !explicitly_versioned_cache_key?(key)
-          super [*key, TemplateDigestor.digest(@virtual_path, formats.last.to_sym, lookup_context)], options, &block
+          super fragment_name_with_digest(key), options, &block
         else
           super
         end
