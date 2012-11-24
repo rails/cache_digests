fc458548df561294c4e3e9e5666a0b88d39fd425
Drew Ulmer <drew.ulmer@gmail.com>
2012-11-24 15:28:33 -0600

    Add skip_digest option key to allow bypassing cache digestion
    In addition to the already present bypass method of passing an explicitly
    version numbered cache key (e.g. ['v1','cache-key']), this adds an option
    to the cache in order to give control in bypassing the digest append step
    on a case-by-case basis.

--- test/fragment_helper_test.rb
+++ test/fragment_helper_test.rb
@@ -9,6 +9,25 @@ class Fragmenter
   end
 end
 
+class BaseFragmenter
+  attr_accessor :virtual_path, :formats, :lookup_context
+  def initialize
+    @virtual_path = ''
+    @formats = [:html]
+  end
+
+  private
+    # Give a base implementation of fragment_for so super calls from
+    # ChildFragmenter have a method to delegate to.
+    def fragment_for(key,opts=nil,&blk)
+      key
+    end
+end
+
+class ChildFragmenter < BaseFragmenter
+  include CacheDigests::FragmentHelper
+end
+
 class FragmentHelperTest < MiniTest::Unit::TestCase
   def setup
     # would love some mocha here
@@ -46,8 +65,28 @@ class FragmentHelperTest < MiniTest::Unit::TestCase
     assert_equal ['key1', 'key2', 'digest'], key_with_digest
   end
 
+  def test_digest_skipped_when_opted_out
+    key = child_fragmenter.send(:fragment_for, 'key1', {skip_digest: true})
+    # 'key1' key derived from super call to BaseFragmenter above
+    assert_equal 'key1', key
+  end
+
+  def test_digest_skipped_when_v_number_keyed
+    key = child_fragmenter.send(:fragment_for, ['v1','key1'])
+    assert_equal ['v1','key1'], key
+  end
+
+  def test_digest_not_skipped_otherwise
+    key_with_digest = child_fragmenter.send(:fragment_for, 'key1', {skip_digest: false})
+    assert_equal ['key1','digest'], key_with_digest
+  end
+
   private
   def fragmenter
     @fragmenter ||= Fragmenter.new
+  end
+
+  def child_fragmenter
+    @child_fragmenter ||= ChildFragmenter.new
   end
 end
