5e3b1c15c618a594fab9f301a6c43e0d2d6e9b25
Daniel Schierbeck <dasch@zendesk.com>
2012-12-09 17:40:41 +0100

    Make the test more readable

--- test/dependency_tracker_test.rb
+++ test/dependency_tracker_test.rb
@@ -15,18 +15,21 @@ class DependencyTrackerTest < MiniTest::Unit::TestCase
     end
   end
 
+  def tracker
+    CacheDigests::DependencyTracker
+  end
+
   def setup
-    CacheDigests::DependencyTracker.register_tracker(:neckbeard, NeckbeardTracker)
+    tracker.register_tracker(:neckbeard, NeckbeardTracker)
   end
 
   def teardown
-    CacheDigests::DependencyTracker.unregister_tracker(:neckbeard)
+    tracker.unregister_tracker(:neckbeard)
   end
 
   def test_finds_tracker_by_template_handler
-    name = "boo/hoo"
-    template = FixtureTemplate.new(name, :neckbeard)
-    dependencies = CacheDigests::DependencyTracker.find_dependencies(name, template)
+    template = FixtureTemplate.new("boo/hoo", :neckbeard)
+    dependencies = tracker.find_dependencies("boo/hoo", template)
     assert_equal ["foo/boo/hoo"], dependencies
   end
 end
